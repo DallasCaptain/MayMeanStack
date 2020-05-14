35bef45a6ab59bc93bbd990bea5aa37c63b74273
Chris Thompson <cthompson@codingdojo.com>
2020-05-14 15:38:18 -0500

    W2D4 Demo - ServerlessAngular

--- MayALgos/rotate.js
+++ MayALgos/rotate.js
@@ -4,3 +4,17 @@
 
 //Unique Letters
 "Snap! Crackle! Poop!"  => "SnCrcklePp"
+
+
+
+//encode
+'aaavveeeeaaarrrttggggg' => 'a3v2e4a3r3t2g5'
+
+'aabbcc' => 'a2b2c2' => 'aabbcc'
+
+//decode
+
+'a3v2e4r3t2g5' => 'aaavveeeerrrttggggg'
+
+'a10' => 'aaaaaaaaaa'
+
---
+++ MayWork/arrows.js
@@ -0,0 +1,13 @@
+var increment = x => x + 1;
+// This works great:
+console.log(increment(3));
+var square = x => {x * x};
+// This is not showing me what I want:
+console.log(square(4));
+// This is not working:
+var multiply = (x,y) => {x * y};
+// Nor is this working:
+var math = (x, y) => let sum = x + y;
+   let product = x * y;
+   let difference = Math.abs(x-y);
+   return [sum, product, difference];
